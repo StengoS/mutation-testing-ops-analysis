ef122ff8f6959dcba1db37cb38cc96713c12a758
Steven Ngo <stengo46@gmail.com>
2022-11-03 23:34:15 -0700

    Generate data for pamvotis functions

--- pamvotis/pamvotis_mutations_data.py
+++ pamvotis/pamvotis_mutations_data.py
@@ -4,13 +4,17 @@ from mutations_stats_script_funcs import *
 
 
 def main():
-    addnode_tests = [1, 2, 3, 4, 5, 6, 7, 8]
-    do_runs("addNode", addnode_tests, "-Dtest=SimulatorAddNodeTest#", "testAddNode",
-        "mutation_matrix/mutations.xml", start_line=1412, end_line=1466, mute_print=False)
+    cov_type = "BRANCH"
+
+    # addnode_tests = [1, 2, 3, 4, 5, 6, 7, 8]
+    # addnode_data = do_runs("addNode", addnode_tests, "-Dtest=SimulatorAddNodeTest#", "testAddNode",
+    #     "mutation_matrix/mutations.xml", cov_type, start_line=1412, end_line=1466, mute_print=False)
+    # generate_mutations_killed_bar_chart(addnode_data, "% Killed in Simulator.addNode()", cov_type)
 
     removenode_tests = [1, 2, 3]
-    do_runs("removeNode", removenode_tests, "-Dtest=SimulatorRemoveNodeTest#", "testRemoveNode",
-        "mutation_matrix/mutations.xml", start_line=1468, end_line=1486, mute_print=False)
+    removenode_data = do_runs("removeNode", removenode_tests, "-Dtest=SimulatorRemoveNodeTest#", "testRemoveNode",
+        "mutation_matrix/mutations.xml", cov_type, start_line=1468, end_line=1486, mute_print=False)
+    generate_mutations_killed_bar_chart(removenode_data, "% Killed in Simulator.removeNode()", cov_type)
     
 
 if __name__ == "__main__":

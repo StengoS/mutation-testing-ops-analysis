a1cee837fa52bfa859922ebe7391b71b2a8e8c00
Steven Ngo <stengo46@gmail.com>
2023-04-17 00:43:22 -0700

    Update collect_mut_data.py with CK code metrics collection, re-run script on big-num repos

--- collect_mut_data.py
+++ collect_mut_data.py
@@ -8,9 +8,9 @@ import csv
 import json
 
 
-## In big-num-arithmetic-<quarter>
+## In big-num-<quarter>
 repo_name = "big-num-arithmetic-###-main"
-anon_id = "F22_##"
+anon_id = "###_##"
 mut_data_dict = {
     "repo_name": repo_name,
     "anon_id": anon_id,
@@ -21,6 +21,7 @@ mut_data_dict = {
 copy_tree("../pit-lib", repo_name + "/lib")
 
 # Add `bignum` package to all files in src/
+# Comment out if already ran before
 java_src_files = [file for file in listdir(repo_name + "/src/") if isfile(join(repo_name + "/src/", file))]
 
 for src_file in java_src_files:
@@ -50,6 +51,10 @@ subprocess.run(
         "--sourceDirs", "src/",
         "--outputFormats", "csv"], 
     shell=True, cwd=repo_name + "/")
+
+# Run CK in cmdline on project, generates class.csv and method.csv in report/
+subprocess.run(["java", "-jar", "../ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar", 
+        "src/", "false", "0", "false", "report/"], shell=True, cwd=repo_name + "/")
 
 # Convert data from individual method to mutation data; mut_data.json file output
 # [ <class name>.java (0), bignum.<class name> (1), <mutation operator> (2), <func name> (3), 
@@ -94,11 +99,40 @@ with open(repo_name + "/report/mutations.csv", "r") as csv_file:
             else:
                 func_info["mutations_not_killed"][mut_op] += 1
 
+column_names = ['file', 'class', 'method', 'constructor', 'line', 'cbo', 'cboModified', 
+                'fanin', 'fanout', 'wmc', 'rfc', 'loc', 'returnsQty', 'variablesQty', 
+                'parametersQty', 'methodsInvokedQty', 'methodsInvokedLocalQty', 
+                'methodsInvokedIndirectLocalQty', 'loopQty', 'comparisonsQty', 'tryCatchQty', 
+                'parenthesizedExpsQty', 'stringLiteralsQty', 'numbersQty', 'assignmentsQty', 
+                'mathOperationsQty', 'maxNestedBlocksQty', 'anonymousClassesQty', 'innerClassesQty', 
+                'lambdasQty', 'uniqueWordsQty', 'modifiers', 'logStatementsQty', 'hasJavaDoc']
 
-    mut_data_json = json.dumps(mut_data_dict, indent = 4)
-    print(mut_data_json)
-    with open(anon_id + "_mut_data.json", "w") as outfile:
-        json.dump(mut_data_dict, outfile)
+used_columns = ["line", "wmc", "rfc", "loc", "returnsQty", "variablesQty", "parametersQty", 
+                "methodsInvokedQty", 'loopQty', 'comparisonsQty', 'tryCatchQty', 
+                'parenthesizedExpsQty', 'stringLiteralsQty', 'numbersQty', 'assignmentsQty', 
+                'mathOperationsQty', 'maxNestedBlocksQty', "uniqueWordsQty"]
+
+# Parse through report/method.csv to match code metrics data to the functions
+with open(repo_name + "/report/method.csv", "r") as csv_file:
+    reader = csv.reader(csv_file)
+
+    for row in reader:
+        class_name = row[1].split(".")[-1]
+        method_name = row[2].split("/")[0]
+        mut_func_name = class_name + "." + method_name
+        
+        if mut_func_name in mut_data_dict["mutated_funcs"]:
+            ck_code_metrics = {}
+            for column in used_columns:
+                index = column_names.index(column)
+                ck_code_metrics[column] = row[index]
+            mut_data_dict["mutated_funcs"][mut_func_name]["ck_code_metrics"] = ck_code_metrics
+
+# Converts mut_data_dict into a .json file
+mut_data_json = json.dumps(mut_data_dict, indent = 4)
+print(mut_data_json)
+with open(anon_id + "_mut_data.json", "w") as outfile:
+    json.dump(mut_data_dict, outfile)
 
 
 """
@@ -113,7 +147,8 @@ JSON DATA TEMPLATE FOR EACH RUN
                     "func_name": String,
                     "total_mutations": int,
                     "mutations_killed": {},
-                    "mutations_not_killed": {}
+                    "mutations_not_killed": {},
+                    "ck_code_metrics": {}
                 },
             <func_name2>:
                 {
